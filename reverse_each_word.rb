db5c0f99f1cb626069ec663e01dd00ab36579cc6
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-07 06:26:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(str)
-  str_array = str.split.str_array.collect do |word|
+  str_array = str.split().str_array.collect do |word|
     word.reverse
   end
   str_array.join(" ")
